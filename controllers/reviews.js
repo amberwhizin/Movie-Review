83932325bb988a9beb8757db283f7a14c18afda6
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-11-01 16:57:00 -0500

    Seed route and fixes

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -37,7 +37,7 @@ critique.get("/:id/edit", (req, res) => {
     res.render("edit.ejs", {
       critique: foundCritique,
     });
-  }); 
+  });
 });
 
 //update
@@ -52,26 +52,57 @@ critique.put("/:id", (req, res) => {
   );
 });
 
-
 //seed
-// critique.get("/seed", async (req, res) => {
-//   const newImage = [
-//     {
-//       title: "face off",
-//       entry: "this movie is the worst and the best",
-//       rating: 5,
-//       img:
-//         "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQuXJdaVQYCMUnTf-0YsMS00IT39lBDkb4CA&usqp=CAU",
-//       hasWatched: true,
-//     },
-//   ];
-//   try {
-//     const seedItems = await Review.create(newImage);
-//     res.send(seedItems);
-//   } catch (err) {
-//     res.send(err.message);
-//   }
-// });
+critique.get("/seed", async (req, res) => {
+  const seedImage = [
+    {
+      title: "Fifth Element",
+      entry: "An ancient alien species is in charge of protecting earth, but are destroyed before they can...except for one. This is an action packed comedy, typical Bruce Willis style film with a touch of romance. ",
+      rating: 5,
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQuXJdaVQYCMUnTf-0YsMS00IT39lBDkb4CA&usqp=CAU",
+      hasWatched: true,
+    },
+    {
+      title: "Face Off",
+      entry: "This movie is the worst and the best, John Travolta and Nicolas Cage literally switch faces and lives. Watch with your friends as these two actors lose their freaking minds. ",
+      rating: 3,
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ20W5H06mwUOwuvIciliClksGx8r7neezvzw&usqp=CAU",
+      hasWatched: true,
+    },
+    {
+      title: "Buffy The Vampire Slayer",
+      entry: "A young woman is chosen to fight demons and save the world. Over and over again. You'll cry, you'll laugh, and scream, with Buffy and all her friends in this horror Horror Dramedy made for all audiences.",
+      rating: 5,
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRg0Z4powVBDQ3DFGARsOQC_J6OcKquWi7suw&usqp=CAU",
+      hasWatched: true,
+    },
+    {
+      title: "Tucker and Dale VS Evil",
+      entry: "Hilarious satire on the teen horror movie genre and hill-billy stereotype.",
+      rating: 5,
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQD8pllsf06LUmPDz2qV1mVdk3NVgXNCiAyqQ&usqp=CAU",
+      hasWatched: true,
+    },
+    {
+      title: "Star Trek: The Next Generation",
+      entry: "An amazingly in-depth look at human nature and life's most difficult philosophical questions. Get ready to have an existential crises with Star Trek: The Next Generation's lovable cast and crew as they explore the Galaxy.",
+      rating: 5,
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6IsxwnCMnbZ6wDMElcOu1TqTYmPbrGJsoHg&usqp=CAU",
+      hasWatched: true,
+    },
+  ];
+  try {
+    const seedItems = await Review.create(seedImage);
+    res.send(seedItems);
+  } catch (err) {
+    res.send(err.message);
+  }
+});
 
 //show
 critique.get("/:id", (req, res) => {
