855556ce3309e63658a2878097b8cde707460171
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-11-01 19:36:37 -0500

    rating limits in model and input edit/new

--- models/reviews.js
+++ models/reviews.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema;
 const TellyReview = new Schema({
   title: { type: String, unique: true, required: true },
   entry: String,
-  rating: { type: Number },
-  img: String, //[]
+  rating: { type: Number, min: 0, max: 5, default: 0, required: true },
+  img: String, //[]?
   hasWatched: {type: Boolean},
 });
 
-const Review = mongoose.model("review", TellyReview);
+const Review = mongoose.model("review", TellyReview); // uh-oh, review, isn't capitalized...
 
 module.exports = Review;
  
