0cf36045d973adced611205012929055fc529993
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-11-03 18:02:55 -0500

    users/new route working

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,11 @@
+const bcrypt = require("bcrypt");
 const express = require("express");
 const users = express.Router();
 const User = require("../models/users.js");
+
+users.get('/new', (req, res) => {
+    //new.ejs is reviews- new.ejs
+    res.render('users/new.ejs')
+})
 
 module.exports = users;
