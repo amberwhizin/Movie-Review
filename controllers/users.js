1196f1f7831f9daa7246d8acd41cee664945af68
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-11-04 17:24:03 -0500

    password hashed and salted

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,25 @@
 const bcrypt = require("bcrypt");
 const express = require("express");
-const User = require("../models/users.js");
-
 const users = express.Router();
+const User = require("../models/users.js");
 
 users.get("/new", (req, res) => {
   //new.ejs is reviews- new.ejs
   res.render("users/new.ejs");
 });
 
+users.post("/", (req, res) => {
+  //post users hashed password
+  req.body.password = bcrypt.hashSync(
+    req.body.password,
+    bcrypt.genSaltSync(10)
+  );
+  //createdUser is coming up undefined, come back to this
+  User.create(req.body, (error, createdUser) => {
+    //console.log("boo", { body: req.body, createdUser, error });
+    res.redirect("/critiques");
+  });
+});
+
+console.log("controller/users.js is linked");
 module.exports = users;
