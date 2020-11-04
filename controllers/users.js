38bca921292b7895d6256b88ae196a87d5342a14
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-11-04 15:42:20 -0500

    partials connected and fixed partials error

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,12 @@
 const bcrypt = require("bcrypt");
 const express = require("express");
-const users = express.Router();
 const User = require("../models/users.js");
 
-users.get('/new', (req, res) => {
-    //new.ejs is reviews- new.ejs
-    res.render('users/new.ejs')
-})
+const users = express.Router();
+
+users.get("/new", (req, res) => {
+  //new.ejs is reviews- new.ejs
+  res.render("users/new.ejs");
+});
 
 module.exports = users;
