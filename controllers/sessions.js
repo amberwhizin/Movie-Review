4a6156b2e88362642ec7cf448d8964a2834b89c3
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-11-04 20:42:21 -0500

    sessions post route complete WIP: messages

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,13 +1,38 @@
 const bcrypt = require("bcrypt");
 const express = require("express");
-const { model } = require("../models/users.js");
-const sessions = express.Router();
 //using my users model!
 const User = require("../models/users.js");
+const sessions = express.Router();
 
-//users new session/logged-in
-sessions.get('/new', (req, res) => {
-    res.send('sessions/new.ejs')
-})
+//users new session/login page
+sessions.get("/new", (req, res) => {
+  res.render("sessions/new.ejs", { currentUser: req.session.currentUser }); //refer to access to user for all gets
+});
 
-module.exports = sessions
+//do username and password match edge-cases
+sessions.post("/", (req, res) => {
+  User.findOne({ username: req.body.username }, (error, foundUser) => {
+    if (error) {
+      res.send('<a href="sessions/new">Oh dear, there was an error </a>');
+    } else if (!foundUser) {
+      res.send('<a href="sessions/new">Username not found</a>');
+      // compare if the foundUser matches the password in dbs
+    } else {
+      if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+        //then set the information that was entered by the user to the foundUser
+        req.session.currentUser = foundUser;
+        res.redirect("/critiques");
+      } else {
+        res.send('<a href="sessions/new">Password incorrect</a>');
+      }
+    }
+  });
+});
+
+sessions.delete("/", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
+});
+
+module.exports = sessions;
